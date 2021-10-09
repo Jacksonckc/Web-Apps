82c57ef4d50f693c057326d10c35963e018557ff
Jacksonckc <jackson858216047@gmail.com>
2021-10-09 17:11:35 -0600

    1

--- Weather_site/Lesson4/js/index.js
+++ Weather_site/Lesson4/js/index.js
@@ -1,20 +1,4 @@
 
-
-function dropdown() {
-        document.getElementById("myDropdown").classList.toggle("show");
-      }
-      
-      // Close the dropdown menu if the user clicks outside of it
-      window.onclick = function(event) {
-        if (!event.target.matches('.dropbtn')) {
-          var dropdowns = document.getElementsByClassName("dropdown-content");
-          var i;
-          for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-              openDropdown.classList.remove('show');
-            }
-          }
-        }
-      
+function drop() {
+  document.getElementById("content").classList.add("mystyle");
 }
