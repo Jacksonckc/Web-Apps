79898f6a4c7bdd6d2f3de8e1932e2abe3efce057
Jacksonckc <jackson858216047@gmail.com>
2021-10-21 09:30:29 -0600

    1

--- Weather_site/Lesson6/js/windchill.js
+++ Weather_site/Lesson6/js/windchill.js
@@ -1,8 +1,17 @@
 function calculate_wind_chill() {
+
     var t = parseInt(document.getElementById('high').innerHTML)
     var s = parseInt(document.getElementById('wind-speed').innerHTML)
     console.log(t)
-    var wind_chill = 35.74+0.6215*t - 35.75*s^(0.16)+0.4275*t*s^(0.16)
+
+    var wind_chill = null
+
+    if (t <= 50 && s > 3){
+        wind_chill = Math.round(35.74+0.6215*t - 35.75 * Math.pow(s, 0.16)+0.4275*t*Math.pow(s, 0.16))}
+
+    else { 
+        wind_chill = 'N/A'
+    }
     document.getElementById('wind-chill').innerHTML = wind_chill
 }
 
