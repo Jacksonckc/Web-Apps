27630428d98544bb5471b2c55856bcd440159b8b
Jacksonckc <jackson858216047@gmail.com>
2021-11-30 09:38:13 -0700

    1

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-document.getElementById("lastUpdated").innerHTML +=
-document.lastModified;
+document.getElementById('lastUpdated').innerHTML += document.lastModified;
 
+window.onload = function () {
+  Particles.init({
+    selector: '.background',
+    color: ['#DA0463', '#404B69', '#DBEDF3'],
+    connectParticles: true,
+  });
+};
+
+var particles = Particles.init({
+  selector: '.background',
+  color: ['#DA0463', '#404B69', '#DBEDF3'],
+  connectParticles: true,
+});
